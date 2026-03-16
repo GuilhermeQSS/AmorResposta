6e609516165a1db73b60bb4c966494f4f1c11502
Guilherme Quintiliano Sobral e Silva <guiqssilva2705@gmail.com>
2026-03-16 09:19:26 -0300

    Organização do projeto 👽

---
+++ backend/models/funcionarioModel.js
@@ -0,0 +1,10 @@
+class Funcionario{
+    constructor(id, nome, usuario, senha, cargo){
+        this.id = id;
+        nome, usuario, senha, cargo
+    }
+
+    getName(params) {
+        
+    }
+}
