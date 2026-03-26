d2b60498c64b77b8b54655b85b81f7b014b46680
guilherme <guilhermeapereiras@gmail.com>
2026-03-25 21:53:26 -0300

    x

--- backend/db/create_documentos.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS documentos (
-    doc_id INT AUTO_INCREMENT PRIMARY KEY,
-    doc_titulo VARCHAR(255) NOT NULL,
-    doc_tipo VARCHAR(100) NOT NULL,
-    doc_data_criacao DATE,
-    doc_descricao TEXT,
-    doc_link VARCHAR(500)
-);
