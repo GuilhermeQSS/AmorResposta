0f37966fc5f106a0dc086dc44d1cd0c2bfd93366
RafaelSF09 <rafaelfiori8@gmail.com>
2026-04-22 23:48:51 -0300

    itens funcionando

--- backend/controllers/itensController.js
+++ backend/controllers/itensController.js
@@ -5,87 +5,84 @@ class ItensController{
     static async listar(req,res){
         try{
             const connection = await SingletonDB.getConnection();
-            let resp = await Funcionario.listar(connection,req.query.filtroNome,req.query.filtroTipo);
+            let resp = await Itens.listar(connection,req.query.nome,req.query.tipo);
             return res.status(200).json(resp);
         }catch(err){
             return res.status(500).json({err: err.message})
         }
     }
 
+    static async buscarPorId(req,res){
+        try{
+            const connection = await SingletonDB.getConnection();
+            let resp = await Itens.buscarPorId(connection,req.query.id);
+            if(!resp){
+                throw new Error(`Não existe item com id = ${req.query.id}`);
+            }else{
+                console.log(resp);
+                return res.status(200).json(resp);
+            }
+        }catch(err){
+            return res.status(500).json({err: err.message});
+        }
+    }
+
     static async alterar(req, res){
         try {
-            if (!req.query.nome || req.query.qtde === undefined || !req.query.tipo) {
-                return res.status(500).json({ 
-                    err: "Algum campo está vazio" ,
-                    campos:{
-                        item_descricao: !descricao,
-                        item_qtde: !qtde
-                    }
-                });
-            }
-            const itens = new Itens(
-                id,
-                descricao,
-                nome,
-                tipo
-            );
             const connection = await SingletonDB.getConnection();
-            let resultado = await itens.alterar(connection);
+            const { id, nome, descricao, tipo, possuiValidade} = req.body;
+            const itemOriginal = await Itens.buscarPorId(connection,id);
+            if(!itemOriginal){
+                throw new Error("Id não existe");
+            }
+            if(itemOriginal.descricao !== descricao &&
+                await Itens.buscarPorNome(connection,descricao)){
+                throw new Error("Item já exite");
+            }
+            const item = new Itens(id,nome,descricao,tipo,possuiValidade);
+            const resultado = await item.alterar(connection);
             return res.status(200).json(resultado);
-        } catch (error) {
-            return res.status(500).json({ erro: "Erro ao alterar itens" });
+        } catch (err) {
+            return res.status(500).json({err: err.message});
         }
     }
 
     static async excluir(req, res){
         try {
-            const { id } = req.body;
-            const itens = new Itens(id);
             const connection = await SingletonDB.getConnection();
-            const resultado = await itens.excluir(connection);
+            let item = await Itens.buscarPorId(connection,req.query.id);
+            if(!item){
+                throw new Error(`Não existe item com id = ${req.query.id}`);
+            }
+            const resultado = await item.excluir(connection);
             res.status(200).json(resultado);
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ erro: "Erro ao excluir itens" });
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json({err: err.message});
         }
     }
     
     static async cadastrar(req,res){
         try{
-            const {descricao, qtde, validade} = req.body;
-            if (qtde < 0) {
+            const {descricao, nome, tipo, possuiValidade} = req.body;
+            if (!nome) {
                 return res.status(500).json({ 
-                    err: "Quantidade inválida",
-                    campos:{item_qtde: qtde}
+                    err: "Nome vazio",
+                    campos:{item_nome: nome}
                 });
             }
-            if (!descricao) {
-                return res.status(500).json({ 
-                    err: "Descrição vazia" ,
-                    campos:{
-                        item_descricao: !descricao
-                    }
-                });
-            }
-            if(validade && new Date(validade) < new Date()){
-                return res.status(500).json({ 
-                    err: "Validade inválida" ,
-                    campos:{
-                        item_descricao: !validade
-                    }
-                });
-            }
-            let itens = new Itens(
+            let item = new Itens(
                 0,
                 descricao,
-                qtde,
-                validade
+                nome,
+                tipo,
+                possuiValidade
             );
             const connection = await SingletonDB.getConnection();
-            let resp = await itens.gravar(connection);
+            let resp = await item.gravar(connection);
             return res.status(200).json(resp);
         }catch(err){
-            return res.status(500).json(err);
+            return res.status(500).json({ err: err.message });
         }
     }
 }
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,4 @@
-import funcionarioRoutes from "./routes/funcionariosRoute.js"
-import beneficiarioRoutes from "./routes/beneficiariosRoute.js"
-import doacaoRoutes from "./routes/doacoesRoute.js"
 import itensRoutes from "./routes/itensRoute.js"
-import encontroRoutes from "./routes/encontrosRoute.js"
 import e from "express";
 import cors from "cors";
 const app = e();
@@ -12,11 +8,7 @@ app.use(cors());
 app.use(e.json());
 app.use(e.urlencoded({extended:true}));
 app.use(e.static("public"));
-app.use("/funcionarios",funcionarioRoutes);
-app.use("/beneficiarios",beneficiarioRoutes);
-app.use("/doacoes",doacaoRoutes);
 app.use("/itens",itensRoutes);
-app.use("/encontros",encontroRoutes);
 
 app.get("/",(req,res)=>{    
     res.redirect("index.html");
