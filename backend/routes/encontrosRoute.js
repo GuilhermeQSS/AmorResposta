944f099fa5cbf19a6f95ea078603dea0d75b7712
Granodek <enzoyoung826@gmail.com>
2026-04-27 07:52:43 -0300

    pequena correção nas Rotas de encontro

--- backend/routes/encontrosRoute.js
+++ backend/routes/encontrosRoute.js
@@ -6,7 +6,6 @@ router.get("/listar",EncontroController.listar);
 router.post("/gravar",EncontroController.cadastrar);
 router.get("/buscar",EncontroController.buscarPorId);
 router.put("/alterar", EncontroController.alterar);
-router.put("/finalizar", EncontroController.finalizarEncontro);
 router.delete("/excluir", EncontroController.excluir);
 
 export default router;
